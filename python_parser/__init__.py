dbdc882897df881971a641d8dea11e8f66f8812b
Elaine Yeung <31430673+egsy@users.noreply.github.com>
2018-11-14 18:53:54 -0800

    Show uploaded file

--- python_parser/__init__.py
+++ python_parser/__init__.py
@@ -71,4 +71,10 @@ def create_app(test_config=None):
           <input type=submit value=Upload>
         </form>
         '''
+    from flask import send_from_directory
+
+    @app.route('/uploads/<filename>')
+    def uploaded_file(filename):
+        return send_from_directory(app.config['UPLOAD_FOLDER'], filename)
+
     return app
